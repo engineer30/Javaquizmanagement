7586d659d7b2f23e9a9e548eb30fed42f8cf01d0
Shruti <Shruti>
2022-10-04 12:11:22 +0530

    Initial commit

---
+++ microproject/src/microproject/Microproject.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class operations extends 
+{
+     public static void main(String[] args) 
+     {
+       Label L1;
+       L1=new Label("Quiz Management system");
+       
+   
+   
+        
+    }
+    
+}
---
+++ src/addNewQuestion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bhagw
+ */
+public class addNewQuestion {
+
+    public addNewQuestion() {
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static class setVisible {
+
+        public setVisible(boolean b) {
+        }
+    }
+    
+}
